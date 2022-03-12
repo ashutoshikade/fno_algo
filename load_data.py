4727f49b923b934e08875a2204d4a91ecb9d9c12
ashutosh <ashutosh@ashutoshs-MacBook-Air.local>
2022-03-12 14:04:07 +0530

    expiry wise lookup path

--- load_data.py
+++ load_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from datetime import date, time, timedelta
+from datetime import date, datetime, time, timedelta
 from ta.trend import SMAIndicator
 
 weekday_idx = {
@@ -7,10 +7,12 @@ weekday_idx = {
     1: "Tuesday",
     2: "Wednesday",
     3: "Thursday",
-    4: "Friday"
+    4: "Friday",
+    5: "Saturday",
+    6: "Sunday"
 }
 
-expiries_lookup = "./expiries_folder_lookup.csv"
+expiries_lookup = "./expiry_folder_lookup.csv"
 
 def atm_strike(cmp):
     '''
@@ -18,12 +20,13 @@ def atm_strike(cmp):
     atm_strike = round(cmp / 50.0) * 50
     return atm_strike
 
-def nearest_expiry(curr_date):
+def nearest_expiry(curr_date, lookup_df):
     '''
     '''
-    expiries = [date(2021, 1, 7), date(2021, 1, 14), date(2021, 1, 21), date(2021, 1, 28)]
+    expiries = lookup_df['Expiries'].to_list()
+    # expiries = [datetime(2021, 1, 7), datetime(2021, 1, 14), datetime(2021, 1, 21), datetime(2021, 1, 28)]
     days_to_expiry = [(i - curr_date) for i in expiries]
-    return expiries[days_to_expiry.index(min([j for j in days_to_expiry if j.days > 0]))]
+    return expiries[days_to_expiry.index(min([j for j in days_to_expiry if j.days >= 0]))]
 
 def aggregate_df(df, delta):
     '''
@@ -63,7 +66,7 @@ def intraday_ohlc(file_path, ondate, dt_format, agg_time=timedelta(minutes=1)):
     spot_df.columns = ['open', 'high', 'low', 'close', 'col1', 'col2', 'datetime']
     spot_df = spot_df[['datetime', 'open', 'high', 'low', 'close', 'col1', 'col2']]
     spot_df['datetime'] = pd.to_datetime(spot_df['datetime'], format=dt_format)
-    intraday_df = spot_df[spot_df['datetime'].dt.date==ondate][spot_df['datetime'].dt.time>=time(9, 15, 0)][spot_df['datetime'].dt.time<=time(15, 15, 0)]
+    intraday_df = spot_df[spot_df['datetime'].dt.date==ondate.date()][spot_df['datetime'].dt.time>=time(9, 15, 0)][spot_df['datetime'].dt.time<=time(15, 15, 0)]
     intraday_agg_df = aggregate_df(intraday_df, agg_time)
     return intraday_agg_df
 
@@ -108,15 +111,21 @@ def squareoff_price_time(df, trade_price, target, r2r):
 def backtest(from_date, to_date, target, r2r, starting_capital=100000, trade_only_on=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]):
     '''
     '''
-    spot_file_path = "./NIFTY_January/IntradayData_JAN2021/NIFTY.txt"
-    option_dir = "./NIFTY_January/Expiry 28th January/"
+    # spot_file_path = "./NIFTY_January/IntradayData_JAN2021/NIFTY.txt"
+    # option_dir = "./NIFTY_January/Expiry 28th January/"
+    path_lookup = pd.read_csv(expiries_lookup)
+    path_lookup['Expiries'] = pd.to_datetime(path_lookup['Expiries'], format="%d/%m/%Y")
     trading_days = 0
     symbol, order_type, buy_time, qtys, buy_price, sell_price, sell_time, pnls, pc_returns, trading_capital = [], [], [], [], [], [], [], [], [], []
     curr_capital = starting_capital
     for i in range((to_date - from_date).days + 1):
         curr_date = from_date + timedelta(days=i)
-        day_of_week = curr_date.weekday
+        day_of_week = curr_date.weekday()
         if weekday_idx[day_of_week] in trade_only_on:
+            expiry_date = nearest_expiry(curr_date, path_lookup)
+            spot_file_path = path_lookup[path_lookup['Expiries'] == expiry_date]['Spot Path'].to_list()[0]
+            option_dir = path_lookup[path_lookup['Expiries'] == expiry_date]['Option Path'].to_list()[0]
+            # print(curr_date, expiry_date, spot_file_path, option_dir)
             agg_spot_df = intraday_ohlc(spot_file_path, curr_date, "%Y%m%d %H:%M", timedelta(minutes=1))
             if len(agg_spot_df) > 0:
                 trading_days += 1
@@ -131,12 +140,12 @@ def backtest(from_date, to_date, target, r2r, starting_capital=100000, trade_onl
                     signal = sma_crossover(curr_agg_spot_df)
                     if signal != None:
                         if signal == 'LONG':
-                            symbol_name = 'NIFTY' + str(atm_strike_ce) + 'CE'
+                            symbol_name = str(atm_strike_ce) + 'CE'
                         else:
-                            symbol_name = 'NIFTY' + str(atm_strike_pe) + 'PE'
+                            symbol_name = str(atm_strike_pe) + 'PE'
                         agg_opt_df = intraday_ohlc(option_dir + symbol_name + '.csv', curr_date, "%Y/%m/%d %H:%M", timedelta(minutes=1))
                         buyprice = agg_opt_df.loc[curr_datetime, 'open']
-                        buyqty = int(curr_capital / (50*buyprice))
+                        buyqty = 2
                         qtys.append(buyqty)
                         symbol.append(symbol_name)
                         order_type.append(signal)
@@ -153,9 +162,9 @@ def backtest(from_date, to_date, target, r2r, starting_capital=100000, trade_onl
                         pc_returns.append(((sellprice - buyprice) / buyprice) * 100)
                     else:
                         continue
-    overall_roi = ((curr_capital - starting_capital) / starting_capital) * 100
-    overall_pc_profitability = ((len(list(filter(lambda x: (x >= 0), pnls)))) / len(pnls)) * 100
-    if overall_roi > 0:
+    if len(pnls) > 0:
+        overall_roi = ((curr_capital - starting_capital) / starting_capital) * 100
+        overall_pc_profitability = ((len(list(filter(lambda x: (x >= 0), pnls)))) / len(pnls)) * 100
         print("Total trades: {}, Trading days: {}, Trades/day: {:.2f}, Profit target: {} pts, R2R: {}, ROI: {:.2f}%, Profitability: {:.2f}%".format(len(pnls), trading_days, (len(pnls)/trading_days), target, r2r, overall_roi, overall_pc_profitability))
     return None
 
@@ -165,11 +174,11 @@ if __name__ == '__main__':
             Day wise. Daily. Monthly. Plot equity curve, drawdown.
             Adjust PnL for brokerage.
     '''
-    from_date = date(2021, 1, 28)
-    to_date = date(2021, 1, 28)
+    from_date = datetime(2021, 1, 1)
+    to_date = datetime(2021, 1, 28)
     tgt_pts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     r2rs = [0.5, 0.667, 0.75, 1, 1.25, 1.33, 1.5, 1.75, 2, 2.25, 2.5, 3]
     for pt in tgt_pts:
         for r2r in r2rs:
-            backtest(from_date, to_date, pt, r2r)
+            backtest(from_date, to_date, pt, r2r, trade_only_on=["Monday", "Tuesday", "Wednesday", "Thursday"])
     # backtest(from_date, to_date, 2, 0.5)
